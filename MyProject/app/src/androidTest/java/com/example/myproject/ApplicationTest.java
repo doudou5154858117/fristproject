c6e8d2063d8ff8b5b3c7e002d017f8fec2be6179
caojian <15256585816@163.com>
2016-03-29 18:27:10 +0800

    第二次提交

--- MyProject/app/src/androidTest/java/com/example/myproject/ApplicationTest.java
+++ MyProject/app/src/androidTest/java/com/example/myproject/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.myproject;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
---
+++ MyProject/app/src/main/java/com/example/myproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 操建 on 2016/3/29.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
