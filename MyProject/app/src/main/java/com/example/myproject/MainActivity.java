5c1c89cf7783acf0dc35bebea378a9c4bde8fd40
caojian <15256585816@163.com>
2016-03-30 18:46:03 +0800

    第四次提交

--- MyProject/app/src/main/java/com/example/myproject/MainActivity.java
+++ MyProject/app/src/main/java/com/example/myproject/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
  */
 public class MainActivity extends Activity{
     @Override
+    //fffffffffffffffffffffffffff
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
